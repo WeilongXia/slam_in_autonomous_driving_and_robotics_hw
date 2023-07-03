b222630987edd04dfee21ce1c547169f246f7efe
XiaWeilong <1017502203@qq.com>
2023-07-03 23:12:40 +0800

    clang format

--- src/ch8/lio-preinteg/lio_preinteg.h
+++ src/ch8/lio-preinteg/lio_preinteg.h
@@ -15,43 +15,48 @@
 #include "common/math_utils.h"
 #include "tools/ui/pangolin_window.h"
 
-namespace sad {
+namespace sad
+{
 
 /**
  * 第8章 基于预积分系统的LIO
  * 框架与前文一致，但之前由IEKF处理的部分变为预积分优化
  */
-class LioPreinteg {
-   public:
+class LioPreinteg
+{
+  public:
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
 
-    struct Options {
-        Options() {}
-        bool with_ui_ = true;  // 是否带着UI
-        bool verbose_ = true;  // 打印调试信息
+    struct Options
+    {
+        Options()
+        {
+        }
+        bool with_ui_ = true; // 是否带着UI
+        bool verbose_ = true; // 打印调试信息
 
-        double bias_gyro_var_ = 1e-2;           // 陀螺零偏游走标准差
-        double bias_acce_var_ = 1e-2;           // 加计零偏游走标准差
-        Mat3d bg_rw_info_ = Mat3d::Identity();  // 陀螺随机游走信息阵
-        Mat3d ba_rw_info_ = Mat3d::Identity();  // 加计随机游走信息阵
+        double bias_gyro_var_ = 1e-2;          // 陀螺零偏游走标准差
+        double bias_acce_var_ = 1e-2;          // 加计零偏游走标准差
+        Mat3d bg_rw_info_ = Mat3d::Identity(); // 陀螺随机游走信息阵
+        Mat3d ba_rw_info_ = Mat3d::Identity(); // 加计随机游走信息阵
 
-        double ndt_pos_noise_ = 0.1;                   // NDT位置方差
-        double ndt_ang_noise_ = 2.0 * math::kDEG2RAD;  // NDT角度方差
-        Mat6d ndt_info_ = Mat6d::Identity();           // 6D NDT 信息矩阵
+        double ndt_pos_noise_ = 0.1;                  // NDT位置方差
+        double ndt_ang_noise_ = 2.0 * math::kDEG2RAD; // NDT角度方差
+        Mat6d ndt_info_ = Mat6d::Identity();          // 6D NDT 信息矩阵
 
-        sad::IMUPreintegration::Options preinteg_options_;  // 预积分参数
-        IncNdt3d::Options ndt_options_;                     // NDT 参数
+        sad::IMUPreintegration::Options preinteg_options_; // 预积分参数
+        IncNdt3d::Options ndt_options_;                    // NDT 参数
     };
 
     LioPreinteg(Options options = Options());
     ~LioPreinteg() = default;
 
     /// init without ros
-    bool Init(const std::string& config_yaml);
+    bool Init(const std::string &config_yaml);
 
     /// 点云回调函数
-    void PCLCallBack(const sensor_msgs::PointCloud2::ConstPtr& msg);
-    void LivoxPCLCallBack(const livox_ros_driver::CustomMsg::ConstPtr& msg);
+    void PCLCallBack(const sensor_msgs::PointCloud2::ConstPtr &msg);
+    void LivoxPCLCallBack(const livox_ros_driver::CustomMsg::ConstPtr &msg);
 
     /// IMU回调函数
     void IMUCallBack(IMUPtr msg_in);
@@ -59,11 +64,11 @@ class LioPreinteg {
     /// 结束程序，退出UI
     void Finish();
 
-   private:
-    bool LoadFromYAML(const std::string& yaml_file);
+  private:
+    bool LoadFromYAML(const std::string &yaml_file);
 
     /// 处理同步之后的IMU和雷达数据
-    void ProcessMeasurements(const MeasureGroup& meas);
+    void ProcessMeasurements(const MeasureGroup &meas);
 
     /// 尝试让IMU初始化
     void TryInitIMU();
@@ -89,12 +94,12 @@ class LioPreinteg {
     StaticIMUInit imu_init_;
 
     /// point clouds data
-    FullCloudPtr scan_undistort_{new FullPointCloudType()};  // scan after undistortion
+    FullCloudPtr scan_undistort_{new FullPointCloudType()}; // scan after undistortion
     CloudPtr current_scan_ = nullptr;
 
     // optimize相关
-    NavStated last_nav_state_, current_nav_state_;  // 上一时刻状态与本时刻状态
-    Mat15d prior_info_ = Mat15d::Identity();        // 先验约束
+    NavStated last_nav_state_, current_nav_state_; // 上一时刻状态与本时刻状态
+    Mat15d prior_info_ = Mat15d::Identity();       // 先验约束
     std::shared_ptr<IMUPreintegration> preinteg_ = nullptr;
 
     IMUPtr last_imu_ = nullptr;
@@ -109,14 +114,14 @@ class LioPreinteg {
     bool flg_first_scan_ = true;
     int frame_num_ = 0;
 
-    MeasureGroup measures_;  // sync IMU and lidar scan
+    MeasureGroup measures_; // sync IMU and lidar scan
     std::vector<NavStated> imu_states_;
-    SE3 TIL_;  // Lidar与IMU之间外参
+    SE3 TIL_; // Lidar与IMU之间外参
 
     Options options_;
     std::shared_ptr<ui::PangolinWindow> ui_ = nullptr;
 };
 
-}  // namespace sad
+} // namespace sad
 
-#endif  // FASTER_LIO_LASER_MAPPING_H
+#endif // FASTER_LIO_LASER_MAPPING_H
